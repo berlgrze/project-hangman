18698dddc82bf5a162f0abdea20d13c15568c4f1
grzegorzberlinski <97801573+grzegorzberlinski@users.noreply.github.com>
2022-01-16 00:06:09 +0100

    Added showWord function

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const answer = document.getElementById('answer');
 
-let autoArray = Array.from('ship');
+let generatedWord = 'someword';
 
 document.addEventListener('keydown', check);
 function check(event){
     let key = event.key;
     const regex = /[A-Za-z]/g;
     const keyMatch = key.match(regex);
-    for(const element of autoArray){
+    for(const element of generatedWord){
         if(element.includes(keyMatch)){
-            console.log('work');
+            console.log('correct letter');
         }
+    }
+}
+
+function showWord(){
+    for(const element of generatedWord){
+        answer.textContent += '*';
     }
 }
